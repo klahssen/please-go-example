fc0a55028373e7678f8f349b3cceb8fc2fc2c74f
Roman Heinrich <roman.heinrich@gmail.com>
2017-04-21 19:23:16 +0200

    install script more robust

--- sh/setup-ci.sh
+++ sh/setup-ci.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-
 # install plz
 curl https://get.please.build | bash
 
 # install go
-sudo add-apt-repository ppa:longsleep/golang-backports
+sudo add-apt-repository ppa:longsleep/golang-backports -y
 sudo apt-get update
 sudo apt-get install golang-go -y
 sudo apt-get install build-essential unzip -y
+
+# install protoc
 pushd /tmp
 wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
-unzip protoc-3.2.0-linux-x86_64.zip
-mv /tmp/bin/protoc /usr/local/bin/
+unzip -o protoc-3.2.0-linux-x86_64.zip
+mv -f /tmp/bin/protoc /usr/local/bin/
 popd
